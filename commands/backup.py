e8fc10aa5d8eb9d7b71f8c337dff673996a5b0bf
Blake Ramsdell <blaker@gmail.com>
2020-11-20 12:13:48 -0800

    Add auto-finding of root for backup

--- commands/backup.py
+++ commands/backup.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import fsutil
 import logging
 import os
 import zipfile
@@ -16,9 +17,13 @@ def backup(args):
         zipdir(args.root, zip_file)
         zip_file.close()
 
+def find_root():
+    return fsutil.find_circuit_python_user_mode_root()
+
 def setup_argument_parser(parser):
     parser.description="Saves a copy of the filesystem on a CircuitPython device."
     default_filename = datetime.now().strftime('backup-%Y-%m-%d-%H-%M-%S%z.zip')
+    root = find_root()
     parser.add_argument("-f", "--filename", action="store", dest="filename", default = default_filename, help="specify the filename to put the backup (default: %(default)s)")
-    parser.add_argument("-r", "--root", action="store", dest="root", help="specify the root directory of your CircuitPython", required=True)
+    parser.add_argument("-r", "--root", action="store", dest="root", default=root, help="specify the root directory of your CircuitPython (default: %(default)s)", required=root is None)
     parser.set_defaults(func=backup)
---
+++ fsutil.py
@@ -0,0 +1,28 @@
+import logging
+import win32api
+
+expected_user_mode_volume_names = [
+    "CIRCUITPY",
+]
+
+# TODO: This is super Windows-centric right now. Other platforms should not
+#       need `win32api` installed and will have their own way of finding the
+#       root directory.
+
+# https://stackoverflow.com/questions/8319264/how-can-i-get-the-name-of-a-drive-in-python
+
+def find_circuit_python_user_mode_root():
+    drives = win32api.GetLogicalDriveStrings()
+    logging.debug("raw drives is %s" % drives)
+    drives = drives.split('\000')[:-1]
+    logging.debug("final list of drives %s" % drives)
+
+    # See if one has a volume name we like
+    for drive in drives:
+        volume_name = win32api.GetVolumeInformation(drive)[0]
+        logging.debug("%s volume name is %s" % (drive, volume_name))
+        if volume_name in expected_user_mode_volume_names:
+            return drive
+
+    logging.debug("did not find a suitable drive")
+    return None
