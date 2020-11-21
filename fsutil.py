44aa807a1debd7e0cc0e23e49cead5f877f69da5
Blake Ramsdell <blaker@gmail.com>
2020-11-21 02:58:53 -0800

    Add find_circuit_python_bootloader_mode_root

--- fsutil.py
+++ fsutil.py
@@ -30,3 +30,19 @@ def find_circuit_python_user_mode_root():
 
     logging.debug("did not find a suitable drive")
     return None
+
+def find_circuit_python_bootloader_mode_root():
+    drives = win32api.GetLogicalDriveStrings()
+    logging.debug("raw drives is %s" % drives)
+    drives = drives.split('\000')[:-1]
+    logging.debug("final list of drives %s" % drives)
+
+    # See if one has a volume name we like
+    for drive in drives:
+        volume_name = win32api.GetVolumeInformation(drive)[0]
+        logging.debug("%s volume name is %s" % (drive, volume_name))
+        if volume_name in expected_bootloader_mode_volume_names:
+            return drive
+
+    logging.debug("did not find a suitable drive")
+    return None
