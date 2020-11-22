1a4e12a43696505cd3c08674193160cf43c3af9d
Blake Ramsdell <blaker@gmail.com>
2020-11-21 20:49:49 -0800

    Fix a problem with double archive extensions

--- commands/backup.py
+++ commands/backup.py
@@ -6,15 +6,16 @@ import shutil
 
 # https://stackoverflow.com/questions/1855095/how-to-create-a-zip-archive-of-a-directory-in-python
 def backup(args):
-    logging.info("Archiving %s to %s" % (args.root, args.filename))
-    shutil.make_archive(args.filename, 'zip', args.root)
+    extension = 'zip'
+    logging.info("Archiving %s to %s.%s" % (args.root, args.filename, extension))
+    shutil.make_archive(args.filename, extension, args.root)
 
 def find_root():
     return fsutil.find_circuit_python_user_mode_root()
 
 def setup_argument_parser(parser):
     parser.description="Saves a copy of the filesystem on a CircuitPython device."
-    default_filename = datetime.now().strftime('backup-%Y-%m-%d-%H-%M-%S%z.zip')
+    default_filename = datetime.now().strftime('backup-%Y-%m-%d-%H-%M-%S%z')
     root = find_root()
     parser.add_argument("-f", "--filename", action="store", dest="filename", default = default_filename, help="specify the filename to put the backup (default: %(default)s)")
     parser.add_argument("-r", "--root", action="store", dest="root", default=root, help="specify the root directory of your CircuitPython (default: %(default)s)", required=root is None)
