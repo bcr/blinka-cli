47242b639ba3d36bb3e0c8be48ed8433b53ab0c1
Blake Ramsdell <blaker@gmail.com>
2020-12-13 01:02:08 -0800

    Font output file writing

--- commands/font.py
+++ commands/font.py
@@ -2,18 +2,19 @@ import fontutil
 import logging
 import unicodedata
 
-def output_glyph(glyph, unicode_codepoint, charset):
-    logging.debug("height = %d, ascent = %d, descent = %d" % (glyph.height, glyph.ascent, glyph.descent))
+def output_glyph(glyph, unicode_codepoint, charset, file):
+    logging.debug("Generating character '%s'" % unicode_codepoint)
     x_offset = glyph.advance_width - glyph.bitmap.width
     y_offset = -glyph.descent if glyph.descent else glyph.ascent - glyph.height
     encoded_char = unicode_codepoint.encode(charset)
     if len(encoded_char) > 0:
         # Bummer. Shouldn't happen for ASCII or 8859-x
         pass
-    print("STARTCHAR %s" % unicodedata.name(unicode_codepoint).replace(' ', '_'))
-    print("ENCODING %d" % encoded_char[0])
-    print("BBX %d %d %d %d" % (glyph.bitmap.width, glyph.bitmap.height, x_offset, y_offset))
-    print("BITMAP")
+    file.write("STARTCHAR %s\n" % unicodedata.name(unicode_codepoint).replace(' ', '_'))
+    file.write("ENCODING %d\n" % encoded_char[0])
+    file.write("DWIDTH %d %d\n" % (glyph.advance_width, 0))
+    file.write("BBX %d %d %d %d\n" % (glyph.bitmap.width, glyph.bitmap.height, x_offset, y_offset))
+    file.write("BITMAP\n")
     for y in range(glyph.height):
         x = 0
         current_byte = 0
@@ -22,15 +23,15 @@ def output_glyph(glyph, unicode_codepoint, charset):
             current_byte |= 1 if glyph.bitmap.pixels[y * glyph.bitmap.width + x] else 0
             x += 1
             if (x % 8) == 0:
-                print("%02X " % current_byte, end='')
+                file.write("%02X " % current_byte)
                 current_byte = 0
 
         remainder = 8 - (x % 8)
         if remainder < 8:
             current_byte <<= remainder
-            print("%02X " % current_byte, end='')
-        print()
-    print("ENDCHAR")
+            file.write("%02X " % current_byte)
+        file.write("\n")
+    file.write("ENDCHAR\n")
 
 def make_suggested_filename(font, size):
     family_name = font.face.family_name.decode("utf-8")
@@ -39,16 +40,31 @@ def make_suggested_filename(font, size):
     final_name = final_name.replace(' ', '-')
     final_name += ".bdf"
     logging.debug("Final filename is %s" % final_name)
+    return final_name
+
+def output_header(file):
+    file.write("STARTFONT 2.1\n")
+    file.write("CHARS\n")
+
+def output_footer(file):
+    file.write("ENDFONT\n")
 
 def do_font(args):
     logging.debug("Making a font from %s %d" % (args.fontpath, args.fontsize))
     font = fontutil.Font(args.fontpath, args.fontsize)
-    make_suggested_filename(font, args.fontsize)
-    unicode_codepoint = 'e'
-    logging.debug("Here is a glyph %s" % unicode_codepoint)
-    glyph = font.glyph_for_character(unicode_codepoint)
-    print(repr(glyph.bitmap))
-    output_glyph(glyph, unicode_codepoint, "ascii")
+    filename = make_suggested_filename(font, args.fontsize)
+    chars_to_output = "Hello, world!"
+    logging.info("Writing font to %s" % filename)
+    with open(filename, "w") as file:
+        output_header(file)
+        char_array = list(chars_to_output)
+        char_set = set(char_array)
+        char_array = list(char_set)
+        char_array.sort()
+        for char in char_array:
+            glyph = font.glyph_for_character(char)
+            output_glyph(glyph, char, "ascii", file)
+        output_footer(file)
 
 def setup_argument_parser(parser):
     parser.description="Perform font management operations."
