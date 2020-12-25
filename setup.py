75310149df12cb3119ff3295a620f1411556fb20
Blake Ramsdell <blaker@gmail.com>
2020-12-25 01:43:56 -0800

    Add JSON resource files to setup

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/bcr/blinka-cli",
     packages=setuptools.find_packages(),
+    package_data={'':['*.json']}, # For grabbing JSON resource files
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
