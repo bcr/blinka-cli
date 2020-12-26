15cfd94f3576f6eac51c1dc1c121cec933c796b5
Blake Ramsdell <blaker@gmail.com>
2020-12-25 16:57:53 -0800

    Switch setup to use new package entrypoint

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 setuptools.setup(
     packages=setuptools.find_packages(),
     entry_points = {
-        'console_scripts': ['blinka=blinka.blinka:main'],
+        'console_scripts': ['blinka=blinka.__main__:main'],
     },
 )
