adb0d44081efee3bb8765379b4c3b7e6afc2357a
Blake Ramsdell <blaker@gmail.com>
2023-05-12 00:48:09 -0700

    Fix missing vid for non-USB serial ports

--- blinka/blinkautil.py
+++ blinka/blinkautil.py
@@ -55,7 +55,7 @@ def reboot_in_bootloader_mode(port):
 
 def find_serial_port():
     for port in serial.tools.list_ports.comports():
-        logging.debug("found port %s vid %04x" % (port, port.vid))
+        logging.debug("found port %s vid %04x" % (port, port.vid or 0))
         if port.vid in vids:
             logging.debug("found matching vid = %04x pid = %04x" % (port.vid, port.pid))
             return port.device
